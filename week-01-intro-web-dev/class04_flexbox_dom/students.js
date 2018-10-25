8ec832efffd5956f9d23e5da02eb5dc66280a20d
martypdx <marty@martypdx.com>
2018-10-25 11:06:39 -0700

    setup

---
+++ week-01-intro-web-dev/class04_flexbox_dom/students.js
@@ -0,0 +1,33 @@
+
+var students = [
+    'paigeegorry',
+    'kristinhortsch',
+    'JenGenRam',
+    'evanandrewson',
+    'deetolman',
+    'avanvalk',
+    'debhardy',
+    'connorfrendt',
+    'CheriPortman',
+    'carmenvramos',
+    'avanvalk',
+    'caripizza',
+    'AdamTHardy',
+    'ajquintero',
+    'AaronD87',
+    'Masmics',
+    'ipoblete',
+    'JeiWolfei',
+    'Zilula',
+    'originalmenagerie',
+    'averagegalmarcy',
+    'pickuppatterns',
+    'intromode',
+    'Shaba9',
+    'tmzaragoza',
+    'tylercorbett',
+    'bpkotowski',
+    'theseus3',
+    'unphurlng'
+];
+  
